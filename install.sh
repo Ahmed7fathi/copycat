1e6b48ee3418ba5ee2170da369a2014a203365dc
ahmed7fathi <ahmed777fathi@gmail.com>
2022-05-05 03:36:48 +0200

    init commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+sudo apt install -y xclip;
+
+zshrc="/home/$USER/.zshrc"
+bashrc="/home/$USER/.bashrc"
+copycat="copycat () {
+  cat \$1;
+  cat \$1 | xclip -selection clipboard;
+}"
+
+if [ -f $zshrc ]; then
+    echo "zshrc exists.";
+    echo "setting up copycat";
+    echo  $copycat | sudo tee -a $zshrc;
+    echo "Done !"
+
+elif [ -f $bashrc ]; then
+    echo "bashrc exists.";
+    echo "setting up copycat";
+    echo  $copycat | sudo tee -a $bashrc;
+    echo "Done !"
+
+else
+    echo "Couldn't find a Shell"
+fi
---
+++ readme.md
@@ -0,0 +1,19 @@
+# copycat
+A small tool depends on xclip package to cat and copy content from files to cliboard
+
+![Alt Text](demo.gif)
+
+
+
+
+
+# Installation
+1. give the Installation file executable permissions
+to edit your shell file and add copycat functionally
+   <br>
+    `sudo chmod +x ./install.sh`
+
+2. Run the Installation <br>
+    `./install.sh`
+
+3. Enjoy !
